fdfd7613aca09e3c9d4558e61ac4e12a7e1b82dc
git <2917021186@qq.com>
2022-06-22 15:05:39 +0800

    Add number

--- chapter15/LeetCode/climb_stairs.cpp
+++ chapter15/LeetCode/climb_stairs.cpp
@@ -2,6 +2,7 @@
 // Created by 晚风吹行舟 on 2022/6/20.
 //
 
+// 70
 // 自顶向下 递归 超出时间限制
 int climb_stairs_top_down(int n) {
     if (n == 1)
@@ -12,13 +13,13 @@ int climb_stairs_top_down(int n) {
 }
 
 // 自底向上法 动态规划
-int climb_stairs_bottom_up(int n){
-    if (n==1||n==2)
+int climb_stairs_bottom_up(int n) {
+    if (n == 1 || n == 2)
         return n;
-    int *arr = new int[n+1];
+    int *arr = new int[n + 1];
     arr[1] = 1;
     arr[2] = 2;
-    for(int i=3;i<=n;i++)
-        arr[i] = arr[i-1]+arr[i-2];
+    for (int i = 3; i <= n; i++)
+        arr[i] = arr[i - 1] + arr[i - 2];
     return arr[n];
 }
